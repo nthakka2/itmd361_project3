57423ade87af3a4718c7ef52cf15100fdf654f3c
NT <nthakkar1@hawk.iit.edu>
2017-12-08 10:01:04 -0600

    second draft
    added automatic slider

--- scripts.js
+++ scripts.js
@@ -1,46 +1,14 @@
-var slideIndex = 1;
+var slideIndex = 0;
+showSlides();
 
-document.addEventListener("DOMContentLoaded", function() {
-  //add var to javascript
-  
-  var leftArrow = document.getElementById("left-arrow");
-  var rightArrow = document.getElementById("right-arrow");
-
-  //add event listener for arrow clicks (how to streamline arrow code)
-  rightArrow.addEventListener("click", plusSlides('next'));
-
-  //add event listener for arrow clicks
-  //leftArrow.addEventListener("click", plusSlides('prev'));
-
-  
-  
-});
-
-// Next/previous controls
-  function plusSlides(direction) {
-    if(direction == 'next') {
-      showSlides(slideIndex += 1);
-    } else {
-      showSlides(slideIndex -= 1);
-    }
-  }
-  
-  // Thumbnail image controls
-  // currentSlide(n) {
-    //showSlides(slideIndex = n);
-  //}
-  
-  function showSlides(n){
-    alert(slideIndex);
+function showSlides() {
     var i;
-    //create array of all slides
-    var slides = document.getElementsByClassName("slide");
-    //select which slide to display
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
+    var slides = document.getElementsByClassName("mySlides");
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
     slides[slideIndex-1].style.display = "block";
-  }
+    setTimeout(showSlides, 3000); 
+} 
